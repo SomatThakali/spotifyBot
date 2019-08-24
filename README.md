0a9711169789673b56a231cab9d42c648a3bb78a
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-08-24 15:32:18 -0400

    Used request-prmise method to request call obkects

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # spotifyBot
 
-spotifyBot is a chatbot app that user can message the song's name, and the Spotifybot will search the artists' name using Spotify API and will message back the user with all the artists' name of that song.
+spotifyBot is a chatbot app that user can message the song's name, and the SpotifyBot will search the artists' name using Spotify API and will message back the user with all the artists' name of that song.
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   Spotify = require("node-spotify-api"),
   keys = require("./keys.js"),
-  spotify = new Spotify(keys.spotify);
+  spotify = new Spotify(keys.spotify),
+  Twilio = require("twilio"),
+  twiml = new Twilio.twiml.MessagingResponse(),
+  request = require("request"),
+  rp = require("request-promise");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,19 +17,22 @@ app.post("/sms", function(req, res) {
     query: req.body.Body,
     limit: 2
   };
-  const Twilio = require("twilio");
-  let twiml = new Twilio.twiml.MessagingResponse();
+  let spotifyURL = spotify.search(searchParams);
 
-  spotify.search(searchParams).then(res => {
-    let tracks = res.tracks.items;
-    tracks.forEach(track => {
-      var artists = track.album.artists;
-      artists.forEach(artist => {
-        twiml.message(artist.name);
-        console.log(artist.name);
+  rp(spotifyURL)
+    .then(function(res) {
+      let tracks = res.tracks.items;
+      tracks.forEach(track => {
+        var artists = track.album.artists;
+        artists.forEach(artist => {
+          twiml.message(artist.name);
+          console.log(artist.name);
+        });
       });
+    })
+    .catch(function(err) {
+      console.log(err);
     });
-  });
 
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
@@ -34,3 +41,5 @@ app.post("/sms", function(req, res) {
 app.listen(1337, function() {
   console.log("Express server listening on port 1337");
 });
+
+// BQByPI1MiE1kH6KexdqthaRpoooiWeSQSEbiav6ie8N0RPEemzujcevSfBvABNGSduKZ2oMmc1-pgDfUwpgePAhZ9NsAAqy3RkpKnjiiwTkSAx7hPJ6fkWW-LSY4suLz3ueNY6sKJwGygngZByOAQhLXieNhBh7joK3DnwPon0PFncr9ruNvYm8VZA
